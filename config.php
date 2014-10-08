91b6848053013ffa0be98a9e767a8abfc32b75f1
Joan Rotbard <jrotbard@virtualmindsoftware.com>
2014-10-07 21:29:28 -0300

    primeros cambios, menu y archivo de configuracion

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+	define (DB_USER, "vennuit");
+	define (DB_PASSWORD, "Greensix6");
+	define (DB_DATABASE, "vennuit");
+	define (DB_HOST, "thunqr.net");
+?>
---
+++ menu.php
@@ -0,0 +1,84 @@
+<header id="header" class="m-animated">
+			<div class="header-bg">
+				<div class="header-inner">
+
+					<!-- HEADER BRANDING : begin -->
+					<div class="header-branding">
+						<a href="./index.html"><img src="./images/logo.png" alt="Vennuit" data-hires="images/logo.2x.png" width="180"></a>
+					</div>
+					<!-- HEADER BRANDING : end -->
+
+					<!-- HEADER NAVIGATION : begin -->
+					<div class="header-navigation">
+
+												<!-- HEADER MENU : begin -->
+						<nav class="header-menu">
+							<button class="header-menu-toggle" type="button"><i class="fa fa-bars"></i>MENU</button>
+							<ul>
+								<li>
+									<span><a href="index.html">Inicio</a></span>
+								</li>
+								<li>
+									<span><a href="comofunciona.html">Como Funciona</a></span>
+								</li>	
+								<li>
+									<span><a href="regalos.html">Gift Cards & Regalos</a></span>
+								</li>
+							<br>
+                            <br>
+                            <br>	
+							</ul>
+						</nav>
+						<!-- HEADER MENU : end -->
+
+						<!-- HEADER CART : begin -->
+						<div class="header-cart">
+							<div class="header-cart-inner">
+								<a href="./shop-cart.html"><i class="cart-ico fa fa-shopping-cart"></i><span class="cart-label">Cart</span><span class="cart-count">(3 items)</span></a>
+							</div>
+						</div>
+						<!-- HEADER CART : end -->
+
+						<!-- HEADER SEARCH : begin -->
+						<div class="header-search">
+							<div class="header-search-inner">
+								<form class="search-form" action="http://demos.volovar.net/beautyspot.html/demo/search-results.html">
+									<i class="search-ico fa fa-search"></i>
+									<input class="search-input" type="text" placeholder="Search for...">
+									<button class="search-submit" type="submit"><i class="fa fa-angle-right"></i></button>
+									<button class="search-toggle" type="button">Search</button>
+								</form>
+							</div>
+							<button class="search-toggle-mobile" type="button"><i class="fa"></i></button>
+						</div>
+						<!-- HEADER SEARCH : end -->
+
+					</div>
+					<!-- HEADER NAVIGATION : end -->
+
+					<!-- HEADER PANEL : begin -->
+					<div class="header-panel">
+
+						<button class="header-panel-toggle" type="button"><i class="fa"></i></button>
+
+						<!-- HEADER RESERVATION : begin -->
+						<div class="header-reservation">
+							<a href="./ajax/reservation-form.html" class="c-button m-open-ajax-modal">Comenzar</a>
+						</div>
+						<!-- HEADER RESERVATION : end -->
+						<!-- HEADER SOCIAL : begin -->
+						<div class="header-social">
+							<ul>
+								<li><a href="#" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+								<li><a href="#" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="#" title="Pinterest"><i class="fa fa-pinterest"></i></a></li>
+							</ul>
+						</div>
+						<!-- HEADER SOCIAL : end -->
+
+					</div>
+					<!-- HEADER PANEL : end -->
+
+				</div>
+			</div>
+		</header>
